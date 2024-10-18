13e8c3e54c9c22b827ddc43a037557c9afc03c06
InTruder-Sec <dhakatedeep909@gmail.com>
2024-10-18 14:55:08 +0530

    fix: changes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -30,6 +30,7 @@ export const getUser = async (req, res) => {
         res.cookie("token", token, {
           maxAge: 7 * 24 * 60 * 60 * 1000,
           sameSite: "none",
+          secure: true,
         });
 
         return res.status(200).json({
